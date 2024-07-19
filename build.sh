0e558b73c70ff7112e62f6541a7d6ea7a879d797
Gabriel Fontán <gabilessto@gmail.com>
2024-07-19 09:09:31 +0200

    Added openjdk

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get install openjdk-11-jdk
 ./gradlew build
 jlink --module-path "$JAVA_HOME"/jmods --add-modules java.base,java.logging --output build/libs/custom-jre
 cp build/libs/com.bobbyesp.spotifylyricsapi-all.jar build/libs/custom-jre/bin/
